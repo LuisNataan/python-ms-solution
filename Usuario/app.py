453364c99563c958d665cd1b484c2a8c5f177e77
LuisNataan <luisnataan556@gmail.com>
2021-05-06 17:08:34 -0300

    Criação dos métodos CRUD em App

---
+++ Usuario/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from DataBase import Database
+import usuario
+
+app = Flask(__name__)
+
+
+def create(self):
+    pass
+
+def update(self):
+    pass
+
+def get_all(self):
+    pass
+
+def get_by_id(self):
+    pass
+
+def delete(self):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
