663a0b62e5da4fb4470a5659c9bbe8fef2e0b4d4
LuisNataan <luisnataan556@gmail.com>
2021-05-07 11:13:33 -0300

    Métodos de rotas de pedido criados e implementados.

--- Pedido/app.py
+++ Pedido/app.py
@@ -4,5 +4,25 @@ from pedido import Pedido
 
 app = Flask(__name__)
 
+@app.route("/cadastrar-pedido/", methods=["POST"])
+def create(self):
+    return Pedido().create()
+
+@app.route("/atualizar-pedido/<id>", methods=["PUT"])
+def update(self):
+    return Pedido().update(id)
+
+@app.route("/listar-pedidos/")
+def get_all(self):
+    return Pedido().get_all()
+
+@app.route("/pedido/<id>")
+def get_by_id(self):
+    return Pedido().get_by_id(id)
+
+@app.route("deletar-pedido/<id>", methods=["DELETE"])
+def delete(self):
+    return Pedido().delete(id)
+
 if __name__ == "__main__":
     app.run(debug=True)
