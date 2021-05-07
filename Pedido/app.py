b305dfcfaab132b4ca953b091655885b3a5e110c
LuisNataan <luisnataan556@gmail.com>
2021-05-07 16:51:30 -0300

    Refatoração e melhoria dos métodos e rotas de Pedido.

--- Pedido/app.py
+++ Pedido/app.py
@@ -6,23 +6,27 @@ app = Flask(__name__)
 
 @app.route("/cadastrar-pedido/", methods=["POST"])
 def create(self):
-    return Pedido().create()
+    values = json.loads(request.data.decode("utf-8"))
+    return Pedido().create(values)
 
-@app.route("/atualizar-pedido/<id>", methods=["PUT"])
+@app.route("/atualizar-pedido/", methods=["PUT"])
 def update(self):
-    return Pedido().update(id)
+    values = json.loads(request.data.decode("utf-8"))
+    return Pedido().update(values)
 
 @app.route("/listar-pedidos/")
 def get_all(self):
     return Pedido().get_all()
 
-@app.route("/pedido/<id>")
+@app.route("/pedido/")
 def get_by_id(self):
-    return Pedido().get_by_id(id)
+    values = json.loads(request.data.decode("utf-8"))
+    return Pedido().get_by_id(values)
 
-@app.route("deletar-pedido/<id>", methods=["DELETE"])
+@app.route("deletar-pedido/", methods=["DELETE"])
 def delete(self):
-    return Pedido().delete(id)
+    values = json.loads(request.data.decode("utf-8"))
+    return Pedido().delete(values)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Pedido/pedido.py
+++ Pedido/pedido.py
@@ -6,16 +6,15 @@ import pandas as pd
 
 class Pedido(Db):
     
-    def create(self):
-        values = json.loads(request.data.decode("utf-8"))
+    def create(self, dicio):
         valor_total = f"{values['Quantidade']*values['Preco']}"
-        
+
         try:
             self.cursor.execute(f"""INSERT INTO pedidos VALUES ({str(uuid4())[0:8]},
                                 {values['Id_Usuario']}, {values['Descricao']}, 
                                 {values['Quantidade']}, {values['Preco']},
-                                {values['valor_total']},
-                                {datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M:%S')}, "Never")""")
+                                {values['valor_total']}, default, default)""")
+            
             return "Pedido realizado.",
             
         except Exception as Error:
@@ -23,16 +22,15 @@ class Pedido(Db):
         return "Não foi possível realizar este pedido.", 400
     
     def update(self, dicio):
-        values = json.loads(request.data.decode("utf-8"))
         sql_list = []
         
-        for key, value in values['set'].items():
-            sql_list.append(f"{key} = '{value}'")
+        for key, value in dicio.items():
+            sql_list.append(f"{key}='{value}'")
         
         try:
-            self.cursor.excecute(f"""UPDATE pedidos SET {', '.join(sql_list)},
-                                 Atualizado_em = '{datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M:%S')}',
+            self.cursor.excecute(f"""UPDATE pedidos SET {', '.join(sql_list)}
                                  WHERE Id= '{dicio['Id']}'""")
+            
             return "Atualizado com sucesso.", 200
             
         except Exception as Error:
@@ -46,21 +44,21 @@ class Pedido(Db):
         
         return df.to_json(orient="records")
     
-    def get_by_id(self, id):
-        values = json.loads(request.data.decode("utf-8"))
-        
+    def get_by_id(self, dicio):
         try:
-            self.cursor.excecute(f"SELECT * FROM pedidos WHERE Id = {id}")
+            self.cursor.excecute(f"SELECT * FROM pedidos WHERE Id = {dicio['Id']}")
+            columns = [i[0] for i in self.cursor.description]
+            df = pd.DataFrame(self.cursor.fetchall(), columns=columns)
+            
+            return df.to_json(orient="records")
         
         except Exception as Error:
             return str(Error.args)
         return "Id inexistente ou inválido.", 404
     
-    def delete(self, id):
-        values = json.loads(request.data.decode("utf-8"))
-        
+    def delete(self, dicio):
         try:
-            self.cursor.excecute(f"DELETE FROM pedidos WHERE Id = {id}")
+            self.cursor.excecute(f"DELETE FROM pedidos WHERE Id = {dicio['Id']}")
             return "Deletado.", 204
             
         except Exception as Error:
