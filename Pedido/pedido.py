968929c8af2f20c7c8bcb309b21de8fc681f2b30
LuisNataan <luisnataan556@gmail.com>
2021-05-07 10:55:14 -0300

    Métodos da classe Pedido implementados.

--- Pedido/pedido.py
+++ Pedido/pedido.py
@@ -1,6 +1,5 @@
 from DataBase.Database import DataBase as Db
 from flask import Flask, request, json
-from bson.json_util import loads, dumps
 from uuid import uuid4
 from datetime import datetime
 import pandas as pd
@@ -8,17 +7,61 @@ import pandas as pd
 class Pedido(Db):
     
     def create(self):
-        pass
+        values = json.loads(request.data.decode("utf-8"))
+        try:
+            self.cursor.execute(f"""INSERT INTO Pedidos VALUES ({str(uuid4())[0:8]},
+                                {values['Id_Usuario']}, {values['Descricao']}, 
+                                {values['Quantidade']}, {values['Preco']},
+                                {values['Quantidade']*values['Preco']},
+                                {datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M:%S')}, "Never")""")
+            return "Pedido realizado.",
+            
+        except Exception as Error:
+            return str(Error.args)
+        return "Não foi possível realizar este pedido.", 400
     
     def update(self, id):
-        pass
+        values = json.loads(request.data.decode("utf-8"))
+        sql_list = []
+        
+        for key, value in values['set'].items():
+            sql_list.append(f"{key} = '{value}'")
+        
+        try:
+            self.cursor.excecute(f"""UPDATE Pedidos SET {', '.join(sql_list)},
+                                 Atualizado_em = '{datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M:%S')}',
+                                 WHERE Id= '{value['Id']}'""")
+            return "Atualizado com sucesso.", 200
+            
+        except Exception as Error:
+            return str(Error.args)
+        return "Não foi possível atualizar.", 400
     
     def get_all(self):
-        pass
+        self.cursor.excecute("SELECT * FROM Pedidos")
+        columns = [i[0] for i in self.cursor.description]
+        df = pd.DataFrame(self.cursor.fetchall(), columns=columns)
+        
+        return df.to_json(orient="records")
     
     def get_by_id(self, id):
-        pass
+        values = json.loads(request.data.decode("utf-8"))
+        
+        try:
+            self.cursor.excecute(f"SELECT * FROM Pedidos WHERE Id = {id}")
+        
+        except Exception as Error:
+            return str(Error.args)
+        return "Id inexistente ou inválido.", 404
     
     def delete(self, id):
-        pass
+        values = json.loads(request.data.decode("utf-8"))
+        
+        try:
+            self.cursor.excecute(f"DELETE * FROM Pedidos WHERE Id = {id}")
+            return "Deletado.", 204
+            
+        except Exception as Error:
+            return str(Error.args)
+        return "Pedido inexistente ou já deletado.", 400
     
